4b8b3809eafc12ecdaea22d7c5e347b6531bee31
Shaojie Li <shaojiel@vmware.com>
2025-01-14 13:02:41 +0800

    2. 两数相加 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
    请你将两个数相加，并以相同形式返回一个表示和的链表。
    
    你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
    
    示例 1：
    
    输入：l1 = [2,4,3], l2 = [5,6,4]
    输出：[7,0,8]
    解释：342 + 465 = 807.
    示例 2：
    
    输入：l1 = [0], l2 = [0]
    输出：[0]
    示例 3：
    
    输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
    输出：[8,9,9,9,0,0,0,1]
    
    提示：
    
    每个链表中的节点数在范围 [1, 100] 内
    0 <= Node.val <= 9
    题目数据保证列表表示的数字不含前导零

---
+++ src/test/java/cn/shaojiel/LeetCode2Test.java
@@ -0,0 +1,57 @@
+package cn.shaojiel;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+class LeetCode2Test {
+
+    @TestFactory
+    Collection<DynamicTest> test() {
+        return List.of(
+                dynamicTest("Case 1", () -> {
+                    LeetCode2.ListNode l1 = new LeetCode2.ListNode(2, new LeetCode2.ListNode(4, new LeetCode2.ListNode(3)));
+                    LeetCode2.ListNode l2 = new LeetCode2.ListNode(5, new LeetCode2.ListNode(6, new LeetCode2.ListNode(4)));
+
+                    LeetCode2.ListNode expected = new LeetCode2.ListNode(7, new LeetCode2.ListNode(0, new LeetCode2.ListNode(8)));
+                    assertThat(LeetCode2.addTwoNumbers(l1, l2)).isEqualTo(expected);
+                }),
+                dynamicTest("Case 2", () -> {
+                    LeetCode2.ListNode l1 = new LeetCode2.ListNode(0);
+                    LeetCode2.ListNode l2 = new LeetCode2.ListNode(0);
+
+                    LeetCode2.ListNode expected = new LeetCode2.ListNode(0);
+                    assertThat(LeetCode2.addTwoNumbers(l1, l2)).isEqualTo(expected);
+                }),
+                dynamicTest("Case 3", () -> {
+                    LeetCode2.ListNode l1 = new LeetCode2.ListNode(9,
+                            new LeetCode2.ListNode(9,
+                                    new LeetCode2.ListNode(9,
+                                            new LeetCode2.ListNode(9,
+                                                    new LeetCode2.ListNode(9,
+                                                            new LeetCode2.ListNode(9,
+                                                                    new LeetCode2.ListNode(9)))))));
+
+                    LeetCode2.ListNode l2 = new LeetCode2.ListNode(9,
+                            new LeetCode2.ListNode(9,
+                                    new LeetCode2.ListNode(9,
+                                            new LeetCode2.ListNode(9))));
+                    
+                    LeetCode2.ListNode expected = new LeetCode2.ListNode(8,
+                            new LeetCode2.ListNode(9,
+                                    new LeetCode2.ListNode(9,
+                                            new LeetCode2.ListNode(9,
+                                                    new LeetCode2.ListNode(0,
+                                                            new LeetCode2.ListNode(0,
+                                                                    new LeetCode2.ListNode(0,
+                                                                            new LeetCode2.ListNode(1))))))));
+                    assertThat(LeetCode2.addTwoNumbers(l1, l2)).isEqualTo(expected);
+                })
+        );
+    }
+}
