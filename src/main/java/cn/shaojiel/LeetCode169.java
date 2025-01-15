2a53750c41667ec334bfcaea13092fc875a81aab
Shaojie Li <shaojiel@vmware.com>
2025-01-15 15:04:40 +0800

    169. 多数元素
    给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
    
    你可以假设数组是非空的，并且给定的数组总是存在多数元素。
    
    示例 1：
    
    输入：nums = [3,2,3]
    输出：3
    示例 2：
    
    输入：nums = [2,2,1,1,1,2,2]
    输出：2
    
    提示：
    n == nums.length
    1 <= n <= 5 * 104
    -109 <= nums[i] <= 109
    
    进阶：尝试设计时间复杂度为 O(n)、空间复杂度为 O(1) 的算法解决此问题。

---
+++ src/main/java/cn/shaojiel/LeetCode169.java
@@ -0,0 +1,54 @@
+package cn.shaojiel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 169. 多数元素
+ *
+ * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ *
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [3,2,3]
+ * 输出：3
+ * 示例 2：
+ *
+ * 输入：nums = [2,2,1,1,1,2,2]
+ * 输出：2
+ *
+ *
+ * 提示：
+ * n == nums.length
+ * 1 <= n <= 5 * 104
+ * -109 <= nums[i] <= 109
+ *
+ *
+ * 进阶：尝试设计时间复杂度为 O(n)、空间复杂度为 O(1) 的算法解决此问题。
+ */
+public class LeetCode169 {
+
+    public static int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+                majorityEntry = entry;
+            }
+        }
+
+        return majorityEntry.getKey();
+    }
+}
--- src/test/java/cn/shaojiel/LeetCode169Test.java
+++ src/test/java/cn/shaojiel/LeetCode169Test.java
@@ -9,17 +9,18 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
-class LeetCode80Test {
+class LeetCode169Test {
+
     @TestFactory
     Collection<DynamicTest> test() {
         return List.of(
                 dynamicTest("Case 1", ()-> {
-                    int[] nums = new int[] {1, 1, 1, 2, 2, 3};
-                    assertThat(LeetCode80.removeDuplicates(nums)).isEqualTo(5);
+                    int[] nums = new int[] {3, 2, 3};
+                    assertThat(LeetCode169.majorityElement(nums)).isEqualTo(3);
                 }),
                 dynamicTest("Case 2", ()-> {
-                    int[] nums = new int[] {0,0,1,1,1,1,2,3,3};
-                    assertThat(LeetCode80.removeDuplicates(nums)).isEqualTo(7);
+                    int[] nums = new int[] {2, 2, 1, 1, 1, 2, 2};
+                    assertThat(LeetCode169.majorityElement(nums)).isEqualTo(2);
                 })
         );
     }
